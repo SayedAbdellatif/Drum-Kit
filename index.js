c88f38e49b659d91fee04dda50deea295631424f
Sayed Abdellatif <github.sayed@gmail.com>
2025-11-05 12:31:56 +1100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+
+let btn = document.querySelectorAll(".drum");
+for(let i = 0; i < btn.length; i++){
+    btn[i].addEventListener("click", function(){
+        btn[i].classList.toggle("letterChange");
+        backgroundChanger();
+        let sound = this.innerHTML;
+        switch(sound){
+            case "w":
+                var audio = new Audio("sounds/tom-1.mp3").play();
+                // audio.play();
+                break;
+            case "a":
+                var audio = new Audio("sounds/tom-2.mp3").play();
+                // audio.play();
+                break;
+            case "s":
+                var audio = new Audio("sounds/tom-3.mp3").play();
+                // audio.play();
+                break;
+            case "d":
+                var audio = new Audio("sounds/tom-4.mp3").play();
+                // audio.play();
+                break;
+            case "j":
+                var audio = new Audio("sounds/crash.mp3").play();
+                // audio.play();
+                break;
+            case "k":
+                var audio = new Audio("sounds/kick-bass.mp3").play();
+                // audio.play();
+                break;
+            case "l":
+                var audio = new Audio("sounds/snare.mp3").play();
+                // audio.play();
+                break;
+            default:
+             console.log("Unknown key:", sound);
+        }
+    })
+}
+
+function backgroundChanger(){
+    var randomColor = Math.floor(Math.random() * 0xffffff).toString(16);
+    var randomChanger = `#${randomColor}`;
+    document.body.style.background = randomChanger;
+    return backgroundChanger;
+}
+console.log(backgroundChanger());
